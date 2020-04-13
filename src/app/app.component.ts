8ecdfd3e46cc9ab0ed95258510210a55dbe50488
Eugeneiregi <eugeneregi2019@gmail.com>
2020-04-13 22:53:28 +0300

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { GithubService } from './services/github/github.service';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  [x: string]: any;
+export class AppComponent {
   title = 'Gitsearch';
-  users: any = [];
-
-  constructor(private githubService: GithubService) { }
-
-   ngOnInit() {
-      this.githubService.getUsers().subscribe((users) => {
-        this.users = users;
-        console.log(this.users);
-      });
-      this.githubService.getUser('Eugeneiregi').subscribe((user) => {
-        console.log(this.user);
-      });
-   }
 }
