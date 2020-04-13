8fb909c25b26921ae42a9755e2db2afb88b3aa49
Eugeneiregi <eugeneregi2019@gmail.com>
2020-04-13 15:26:02 +0300

    Fix On init Erfrors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GithubService } from './services/github/github.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Gitsearch';
 
   constructor(private githubService: GithubService) { }
+
+   ngOnInit() {
+
+   }
 }
